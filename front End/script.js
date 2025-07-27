181c50005cf843067aa667c10efe4e2b2e00670a
Tarapada Garai <146563644+codingWithRakesh@users.noreply.github.com>
2025-07-27 22:13:35 +0530

    Add module script support and update page scripts for consistency

--- front End/script.js
+++ front End/script.js
@@ -1,1 +1,3 @@
 console.log("working");
+
+export const check = "working";
--- front End/scripts/profile.js
+++ front End/scripts/profile.js
@@ -1,1 +1,4 @@
+import { check } from '../script.js';  // Correct path to the file where `check` is exported
+
 console.log("Profile Page Loaded");
+console.log(check);
