cdd01064a7300d073a89084af7d26986604db79b
Tarapada Garai <146563644+codingWithRakesh@users.noreply.github.com>
2025-10-17 00:26:51 +0530

    fix: Update SERVER_URL to use production endpoint

--- front End/constants.js
+++ front End/constants.js
@@ -1,2 +1,2 @@
-export const SERVER_URL = "http://localhost:8000/api/v1";
-// export const SERVER_URL = "https://x-clone-server-dot.vercel.app/api/v1";
+// export const SERVER_URL = "http://localhost:8000/api/v1";
+export const SERVER_URL = "https://x-clone-server-dot.vercel.app/api/v1";
